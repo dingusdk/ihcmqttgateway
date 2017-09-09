e1bea76896b045c4142ef41321a2dd6eb3463b2a
Jens <jensoen@gmail.com>
2017-09-09 13:06:32 +0200

    Initial commit

---
+++ Configuration.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace IhcMqttGateway {
+
+	/// <summary>
+	/// The class has the configuration properties and 2 dictionaries for the 
+	/// mapping of ihc ids to mqtt topics
+	/// </summary>
+	public class Configuration {
+
+		public string IhcUrl { get; protected set; }
+		public string IhcUser { get; protected set; }
+		public string IhcPassword { get; protected set; }
+		public string MqttHost { get; protected set; }
+
+		public string BooleanFalse { get; protected set; }
+		public string BooleanTrue { get; protected set; }
+
+		public Dictionary< string,int> IhcIn{ get; protected set; }
+		public Dictionary< int, string> IhcOut { get; protected set; }
+
+		public Configuration() {
+
+			IhcIn = new Dictionary<string, int>();
+			IhcOut = new Dictionary<int, string>();
+			BooleanFalse = "0";
+			BooleanTrue = "1";
+		}
+
+		/// <summary>
+		/// Load the configuration from a file
+		/// </summary>
+		/// <param name="path">Configuration file</param>
+		/// <returns>True if configuration was loaded successfully</returns>
+		public bool Load(string path) {
+
+			if (!File.Exists(path)) {
+				Console.Error.WriteLine("Configuration file not found: " + path);
+				return false;
+			}
+			using (var file = new FileStream(path, FileMode.Open, FileAccess.Read)) {
+				using (var reader = new StreamReader(file)) {
+					var properties = GetType().GetProperties();
+					while (!reader.EndOfStream) {
+						var line = reader.ReadLine().Trim();
+						// Comment line?
+						if (String.IsNullOrEmpty(line) || line.StartsWith("//") || line.StartsWith("#")) continue;
+						if (char.IsDigit(line[0])) {
+							bool input = false;
+							bool output = false;
+							int pos = line.IndexOf("<-");
+							int pos3 = 0;
+							if (pos > 0) {
+								input = true;
+								pos3 = pos + 2;
+							}
+							int pos2 = line.IndexOf("->");
+							if (pos2 > 0) {
+								if (pos < 0) pos = pos2;
+								output = true;
+								pos3 = pos2 + 2;
+							}
+							if (pos < 0) {
+								Console.Error.WriteLine("Error in line: " + line);
+								continue;
+							}
+							string id = line.Substring(0, pos).TrimEnd();
+							int ihcid = int.Parse(id);
+							string mqttmsg = line.Substring(pos3).Trim();
+							if (input)
+								IhcIn.Add(mqttmsg, ihcid);
+							if (output)
+								IhcOut.Add(ihcid, mqttmsg);
+							continue;
+						}
+						// We will find the associated configuration property at set the value
+						var s = line.Split(' ');
+						string name = s[0].Trim();
+						var p = properties.FirstOrDefault(x => String.Equals(x.Name, name, StringComparison.InvariantCultureIgnoreCase));
+						if (p == null) {
+							Console.Error.WriteLine("Error in line: " + line);
+							continue;
+						}
+						p.SetValue(this, s[1].Trim());
+					}
+				}
+			}
+			return true;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,172 @@
+﻿using Dingus.IHCSdkWR;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace IhcMqttGateway {
+
+	class Program {
+
+		static bool verbose = false;
+		static string confpath = "ihcmqttgateway.conf";
+
+		static Configuration conf = new Configuration();
+		static IHCController ihc;
+		static MqttClient mqttclient;
+
+		static Dictionary<int, string> ihcstates;
+		static Dictionary<int, string> mqttstates;
+		static Dictionary<int, Type> ihctypes;
+
+
+		static void Main(string[] args) {
+
+			Console.WriteLine( String.Format( "IhcMqttGateway {0} (c) 2017 J.Ø.N. Dingus.dk", GetVersion()));
+			if (!ProcessCmdLine(args)) return;
+			if (!conf.Load( confpath)) {
+				Console.Error.WriteLine("Error loading the configuration: ihcmqttgateway.conf");
+				return;
+			}
+
+			ihcstates = new Dictionary<int, string>();
+			mqttstates = new Dictionary<int, string>();
+			ihctypes = new Dictionary<int, Type>();
+
+			Console.WriteLine("Connectiong to ihc controller: " + conf.IhcUrl);
+			ihc = new IHCController( conf.IhcUrl);
+			if (!ihc.Authenticate( conf.IhcUser,conf.IhcPassword)) {
+				Console.WriteLine("Authentication failed");
+				return;
+			}
+			Console.WriteLine("Authentication succecfull");
+
+			foreach( var i in conf.IhcOut) {
+				ihc.RegisterNotify(i.Key, ResourceChanges);
+			}
+			mqttclient = new MqttClient( conf.MqttHost);
+			mqttclient.MqttMsgPublishReceived += MqttMsgPublishReceived;
+			string clientId = Guid.NewGuid().ToString();
+			Console.WriteLine("Conneting to MQTT");
+			mqttclient.Connect(clientId);
+			Console.WriteLine("Subscribe to MQTT");
+			// Subscribe to all topics!
+			mqttclient.Subscribe(new string[] { "#" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+			ihc.StartNotify();
+			Console.WriteLine("READY  (press 'q' to exit)");
+
+			while (Console.ReadKey().KeyChar != 'q') { }
+
+			Console.WriteLine("\r\nDisconnecting");
+			mqttclient.Disconnect();
+			ihc.Disconnect();
+		}
+
+		static void MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) {
+
+			try {
+				if (!conf.IhcIn.ContainsKey(e.Topic)) return;
+				int ihcid = conf.IhcIn[e.Topic];
+				string value = Encoding.ASCII.GetString(e.Message);
+				if ( verbose)
+					Console.Out.WriteLine("MQTT: " + e.Topic + " -> " + value);
+				mqttstates[ihcid] = value;
+				if (!ihctypes.ContainsKey(ihcid)) {
+					var values = ihc.GetRuntimeValues(new int[] { ihcid });
+					var v = values[0];
+					ihctypes[ ihcid] = v.GetType();
+					ihcstates[ihcid] = ValueToString(v);
+				}
+				if (ihcstates.ContainsKey(ihcid) && ihcstates[ihcid] == value) return;
+				ihcstates[ihcid] = value;
+				switch (ihctypes[ihcid].Name) {
+					case "Boolean":
+						ihc.SetRuntimeValueBool(ihcid, value == conf.BooleanTrue);
+						break;
+					case "Int32":
+						ihc.SetRuntimeValueInt(ihcid, int.Parse( value));
+						break;
+					case "Double":
+					case "Single":
+						ihc.SetRuntimeValueFloat(ihcid, float.Parse(value));
+						break;
+				}
+			}
+#pragma warning disable CS0168
+			catch (Exception ex) { 
+			}
+
+		}
+
+		static private void ResourceChanges( int ihcid,object v) {
+
+			try {
+				if (!conf.IhcOut.ContainsKey(ihcid)) return;
+				if (!ihctypes.ContainsKey(ihcid)) {
+					ihctypes[ihcid] = v.GetType();
+				}
+				string value = ValueToString( v);
+				if (verbose)
+					Console.WriteLine("IHC change: " + ihcid.ToString() + "->" + value);
+				string msg = conf.IhcOut[ihcid];
+				ihcstates[ihcid] = value;
+				if (mqttstates.ContainsKey(ihcid) && mqttstates[ihcid] == value) return;
+				mqttstates[ihcid] = value;
+				mqttclient.Publish(msg, Encoding.ASCII.GetBytes(value));
+			}
+#pragma warning disable CS0168
+			catch (Exception e) {
+			}
+		}
+
+		static String ValueToString(  object value) {
+
+			if (value is bool) {
+				bool b = (bool)value;
+				return b ? conf.BooleanTrue : conf.BooleanFalse;
+			}
+			return value.ToString();
+		}
+
+
+		static private bool ProcessCmdLine(string[] args) {
+
+			if (args.Length == 0) return true;
+			int i = 0;
+			if (args[0].StartsWith("-")) {
+				if (args[0] == "-v") {
+					verbose = true;
+					i++;
+				}
+				else {
+					Console.Error.WriteLine("Unknown option");
+					Help();
+					return false;
+				}
+			}
+			if (args.Length <= i) return true;
+			confpath = args[i];
+			return true;
+		}
+
+		static private void Help() {
+
+			Console.WriteLine("Syntax:");
+			Console.WriteLine("IhcMqttGateway.exe [-v] pathtoconffile");
+			Console.WriteLine("-v option for extra logging (IHC or Mqtt changes)");
+
+		}
+		static private string GetVersion() {
+			System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+			FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+			return fvi.FileVersion;
+		}
+
+	}
+}
